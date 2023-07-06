d82d3fc94fbfb662eb3cea0b509ec58e3eee713a
Issac Willian <135528719+issacwillianDEV@users.noreply.github.com>
2023-07-05 21:19:20 -0300

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,205 @@
+ScrollReveal().reveal('#nossamissaoheading', {
+    origin: 'left',
+    distance: '100px',
+    duration: 400,
+    delay: 400
+});
+
+ScrollReveal().reveal('#subtitulonossamissao', {
+    origin: 'right',
+    distance: '100px',
+    duration: 400,
+    delay: 400
+});
+
+ScrollReveal().reveal('#imagempadre', {
+    origin: 'right',
+    distance: '100px',
+    duration: 500,
+    delay: 500
+});
+
+
+
+
+
+ScrollReveal().reveal('#missaoevangelismo', {
+    origin: 'left',
+    distance: '130px',
+    duration: 500,
+    delay: 500
+});
+
+ScrollReveal().reveal('#missaoevangelismoparagraph', {
+    origin: 'right',
+    distance: '50px',
+    duration: 500,
+    delay: 500
+});
+
+
+
+ScrollReveal().reveal('#missaoevangelismo-1', {
+    origin: 'left',
+    distance: '130px',
+    duration: 500,
+    delay: 500
+});
+
+ScrollReveal().reveal('#missasregularesparagraph', {
+    origin: 'right',
+    distance: '110px',
+    duration: 500,
+    delay: 500
+});
+
+
+
+ScrollReveal().reveal('#apaixonadospeloreiono-1', {
+    origin: 'left',
+    distance: '130px',
+    duration: 500,
+    delay: 500
+});
+
+ScrollReveal().reveal('#apaixonadospeloreiono', {
+    origin: 'right',
+    distance: '110px',
+    duration: 500,
+    delay: 500
+});
+
+
+
+ScrollReveal().reveal('#trabalhomissionariohaiti', {
+    origin: 'right',
+    distance: '130px',
+    duration: 600,
+    delay: 600
+});
+
+ScrollReveal().reveal('#letratrabalhomissionario', {
+    origin: 'right',
+    distance: '110px',
+    duration: 600,
+    delay: 600
+});
+
+
+ScrollReveal().reveal('#video1', {
+    origin: 'left',
+    distance: '110px',
+    duration: 500,
+    delay: 500
+});
+
+ScrollReveal().reveal('#video2', {
+    origin: 'left',
+    distance: '110px',
+    duration: 600,
+    delay: 600
+});
+
+ScrollReveal().reveal('#video3', {
+    origin: 'right',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+ScrollReveal().reveal('.imgtopo1', {
+    origin: 'right',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+ScrollReveal().reveal('.subtitulo1', {
+    origin: 'left',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+ScrollReveal().reveal('.santuariosaomiguel', {
+    origin: 'right',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+
+
+ScrollReveal().reveal('.imgtopo2', {
+    origin: 'right',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+ScrollReveal().reveal('.subtitulo2', {
+    origin: 'left',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+ScrollReveal().reveal('.saomiguelsant', {
+    origin: 'right',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+
+
+
+
+ScrollReveal().reveal('.imgtopo3', {
+    origin: 'right',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+ScrollReveal().reveal('.subtitulo4', {
+    origin: 'left',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+ScrollReveal().reveal('.santuarioimg1', {
+    origin: 'right',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+
+
+
+ScrollReveal().reveal('.imgtopo4', {
+    origin: 'right',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+ScrollReveal().reveal('.subtitulo5', {
+    origin: 'left',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+ScrollReveal().reveal('.churchabbapai', {
+    origin: 'right',
+    distance: '110px',
+    duration: 550,
+    delay: 550
+});
+
+
+
 
